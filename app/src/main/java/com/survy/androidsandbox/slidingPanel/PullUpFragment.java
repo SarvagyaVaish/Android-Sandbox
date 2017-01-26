6b8d43876f9121c54d99ad9f7a3f1129f264e353
Survy <sarvagya.vaish7@gmail.com>
2017-01-26 09:28:24 -0500

    Using logs to understand fragment lifecycle

--- app/src/main/java/com/survy/androidsandbox/slidingPanel/PullUpFragment.java
+++ app/src/main/java/com/survy/androidsandbox/slidingPanel/PullUpFragment.java
@@ -1,19 +1,17 @@
 package com.survy.androidsandbox.slidingPanel;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.survy.androidsandbox.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class PullUpFragment extends Fragment {
 
+    final static private String TAG = "PullUpFragment";
 
     public PullUpFragment() {
         // Required empty public constructor
@@ -25,6 +23,36 @@ public class PullUpFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pull_up, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() " + this.toString());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() " + this.toString());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() " + this.toString());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() " + this.toString());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() " + this.toString());
     }
 
 }
